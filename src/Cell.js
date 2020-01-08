064006fb018985e3c07c431218a155c7463e814c
Frank Gerold <FrankETSGerold@gmail.com>
2020-01-08 09:21:54 -0500

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Cell extends Component {
+  state = {
+    color: this.props.value
+  }
+
+  render() {
+    return (
+      <div className='cell' style={{backgroundColor: this.state.color}} onClick={this.handleClick}></div>
+    );
+  }
+
+  handleClick = () => {
+    this.setState({color: '#333'})
+  }
+}
+
+export default Cell;
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import Cell from './Cell'
 
 export default class Matrix extends Component {
-  
+
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    vals.map(val => <Cell value={val} />) // replace me and render a cell component instead!
   )
-  
+
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
-  
+
   render() {
     return (
       <div id="matrix">
@@ -17,5 +18,22 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+
+
+}
+
+Matrix.defaultProps = {
+  values: dataGen('#F00')
+}
+
+function dataGen (value)  {
+  let outer = [];
+  for (let y = 0; y < 10; y++) {
+    let row = []
+    for (let x = 0; x < 10; x++) {
+      row.push(value)
+    }
+    outer.push(row)
+  }
+  return outer
 }
